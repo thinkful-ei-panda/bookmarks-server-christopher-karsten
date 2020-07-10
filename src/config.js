aa67672364d2d587423c8c764b89e10e1203849b
Karsten Taylor <karstentaylor0@gmail.com>
2020-07-10 13:16:49 -0700

    finished route handlers

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || 'e89097df-b4ef-4fd5-b1c3-c6dcc5572660',
 };
